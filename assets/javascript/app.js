b869aec728331d135bce06317c3efc6c46d6e327
John Laceda <john.laceda@gmail.com>
2019-03-31 22:17:25 -0700

    Allow users to request additional gifs to be added.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,8 +17,34 @@ const gifTemplate = ({title, rating, still_url, gif_url}) => `
 const animalButtonClickHandler = (event) =>
 {
 	let animal = $(event.currentTarget).text().trim();
-	console.log(animal);
-	const queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=PETvFqyOmcpUHrypnSUhr1F7RHLIjF49&q='+animal+'&limit=10';
+	let queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=PETvFqyOmcpUHrypnSUhr1F7RHLIjF49&limit=10&q=' + animal;
+	// make arrays of animals and counts;
+	let animalArray = [];
+	let countArray = [];
+	for (let i = 0; i < gifs.length; i++) {
+		const gifAnimal = gifs[i].animal;
+		// if this animal is NOT already in animalArray, add it with count 1
+		const gifAnimalIndex = animalArray.indexOf(gifAnimal);
+		if (gifAnimalIndex === -1)
+		{
+			animalArray.push(gifAnimal);
+			countArray.push(1);
+		}
+		// if not, add 1 to count.
+		else
+		{
+			countArray[gifAnimalIndex] += 1;
+		}
+	}
+	// if there's already gifs of this animal
+	// add an offset to the request
+	const animalIndex = animalArray.indexOf(animal);
+	if (animalIndex !== -1)
+	{
+		let offset = countArray[animalIndex]/10;
+		queryUrl += '&offset=' + offset;
+	}
+	console.log(queryUrl);
 	$.ajax({
 		url: queryUrl,
 		method: "GET"
@@ -37,9 +63,6 @@ const animalButtonClickHandler = (event) =>
 				});
 				
 			});
-			console.log(dataArray);
-			console.log(gifs);
-			//gifs = Array.from(new Set(gifs));
 			renderGifs(animal);
 		}
 	});
@@ -60,8 +83,9 @@ const renderAnimalButtons = () =>
 	buttonsDiv.find(".animalButton").click(animalButtonClickHandler);
 }
 
-$("#addAnimalButton").click(function()
+$("#addAnimalButton").click(function(event)
 {
+	event.preventDefault();
 	let animalInput = $("#animalInput");
 	let animal = animalInput.val().trim().toLowerCase();
 	if (animal.length > 0)
